e101c2021480b78eb644c2a369ab165b3e4b3b6b
Francisco Guerrero <fjgg19@gmail.com>
2019-10-05 17:28:19 -0300

    creacion de proyecto

---
+++ meli-mobile-candidate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  meli-mobile-candidate
+//
+//  Created by Francisco Guerrero on 05/10/2019.
+//  Copyright © 2019 Francisco Guerrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ meli-mobile-candidateTests/meli_mobile_candidateTests.swift
@@ -0,0 +1,34 @@
+//
+//  meli_mobile_candidateTests.swift
+//  meli-mobile-candidateTests
+//
+//  Created by Francisco Guerrero on 05/10/2019.
+//  Copyright © 2019 Francisco Guerrero. All rights reserved.
+//
+
+import XCTest
+@testable import meli_mobile_candidate
+
+class meli_mobile_candidateTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
